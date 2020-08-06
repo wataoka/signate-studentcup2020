5148b8b96635c2dda197fbb1fc8924b83146dc59
wataoka <wataoka@stu.kobe-u.ac.jp>
2020-08-06 21:23:33 +0900

    add: run_jupyter_server.sh in scripts

---
+++ scripts/run_jupyter_server.sh
@@ -0,0 +1,5 @@
+# If you execute this file, you copy and paste localhost url to google colab in google drive.
+jupyter notebook \
+  --NotebookApp.allow_origin='https://colab.research.google.com' \
+  --port=8888 \
+  --NotebookApp.port_retries=0
