fd6f89aeb0dbc00810be04e85b82079cb22253fa
wataoka <wataoka@stu.kobe-u.ac.jp>
2020-08-06 10:51:27 +0900

    add: fix_seed in utils/utils.py

---
+++ utils/utils.py
@@ -0,0 +1,13 @@
+import os
+import random
+
+import torch
+import numpy as np
+
+def fix_seed(seed=1):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
