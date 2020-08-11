106cd2fe1f9f2b901879949ede403a8c037b9ecb
wataoka <wataoka@stu.kobe-u.ac.jp>
2020-08-11 22:30:02 +0900

    add: word2vec in utils/features.py

--- utils/features.py
+++ utils/features.py
@@ -1,11 +1,15 @@
 import os
 import sys
+import pickle
 from tqdm import tqdm
 from collections import defaultdict
 
 import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+
+sys.path.append('..')
+from constants import REPRESENTATIONS_DIR
 
 def tfidf(train_df, test_df, max_features=None):
 
@@ -117,5 +121,56 @@ def importance(train_df, test_df):
     print('Creating test data...')
     for word in tqdm(important_words):
         test_data[word] = test_df['description'].apply(count, target=word)
+    
+    return train_data, test_data
+
+def word2vec(train_df, test_df, mode='mean'):
+
+    filename = os.path.join(REPRESENTATIONS_DIR, 'word2vec.pkl')
+    with open(filename, mode='rb') as f:
+        word2vec = pickle.load(f)
+
+    def get_vec_list(desc):
+        vec_list = []
+        for word in desc.split(' '):
+            if len(word) == 0 or word == '.':
+                continue
+            if word not in word2vec.keys():
+                continue
+            if word[-1] in ['.', ',', ';', ')']:
+                word = word[:-1]
+            if len(word) == 0:
+                continue
+            if word[0] in ['(']:
+                word = word[1:]
+            vec_list.append(word2vec[word])
+        vec_list = np.array(vec_list)
+        return vec_list
+
+    train_descriptions = train_df['description'].values.tolist()
+    train_data = pd.DataFrame([])
+    print('Creating train data...')
+    for description in tqdm(train_descriptions):
+        vec_list = get_vec_list(description)
+        if mode == 'mean':
+            vec = vec_list.mean(axis=0)
+        else:
+            print(f'InvalidMode: {mode} is not supported in word2vec.')
+            exit(1)
+        vec = pd.DataFrame(vec.reshape(1, len(vec)))
+        train_data = pd.concat([train_data, vec], axis=0)
+
+    test_descriptions = test_df['description'].values.tolist()
+    test_data = pd.DataFrame([])
+    print('Creating test data...')
+    for description in tqdm(test_descriptions):
+        vec_list = get_vec_list(description)
+        if mode == 'mean':
+            vec = vec_list.mean(axis=0)
+        else:
+            print(f'InvalidMode: {mode} is not supported in word2vec.')
+            exit(1)
+        vec = pd.DataFrame(vec.reshape(1, len(vec)))
+        test_data = pd.concat([test_data, vec], axis=0)
     
     return train_data, test_data
