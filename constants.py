cc90d42db53bd793e2c7660238303f8c46394a4b
wataoka <wataoka@stu.kobe-u.ac.jp>
2020-08-14 20:16:06 +0900

    add: PRETRAINED_DIR in constants

--- constants.py
+++ constants.py
@@ -4,6 +4,7 @@ HOME_DIR: str = os.path.expanduser('~')
 HERE_DIR: str = os.path.dirname(os.path.abspath(__file__))
 
 DATASET_DIR: str = os.path.join(HOME_DIR, 'datasets', 'signate-studentcup2020')
+PRETRAINED_DIR: str = os.path.join(HOME_DIR, 'pretrained')
 
 BASE_DIR: str = HERE_DIR
 UTILS_DIR: str = os.path.join(BASE_DIR, 'utils')
