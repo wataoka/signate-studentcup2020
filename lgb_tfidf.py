a608a25c063e7362a48e144068ff501cbeef6a5e
wataoka <wataoka@stu.kobe-u.ac.jp>
2020-08-25 21:01:40 +0900

    change: verbose_eval from 50 to 10 in lgb_tfidf.py

--- lgb_tfidf.py
+++ lgb_tfidf.py
@@ -67,7 +67,7 @@ if __name__ == "__main__":
                           valid_sets=[train_data,valid_data],
                           num_boost_round=num_boost_round,
                           early_stopping_rounds=early_stopping_rounds,
-                          verbose_eval=50,
+                          verbose_eval=10,
                           feval=metric_f1)
         
         # evaluate
